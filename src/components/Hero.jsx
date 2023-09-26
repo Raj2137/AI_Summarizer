0c2aee59ccbf1ec82c2d80af6a99f873e9a6b2f5
Raj Kumar Rana <105447781+Raj2137@users.noreply.github.com>
2023-09-26 20:08:31 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero
       </nav>
       <h1 className='head_text'> Summarize Articles with <br/> <span className='orange_gradient'>OpenAI GPT-4</span></h1>
       <h2 className='desc'>
-        Simply your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries
+        simplify your reading with Summize, an open AI article summarizer that transforms lengthy articles into clear and concise summaries
       </h2>
     </header>
   )
